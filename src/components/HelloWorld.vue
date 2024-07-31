71001685adabdb0834790e48a625827b873bf8a8
Reuben Stander <Yodiat32@outlook.com>
2024-07-31 13:46:53 +0200

    startup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="fill-height">
+    <v-btn @click="doSomething">
+  My Button
+</v-btn>
+{{ myAction }}
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const myAction = ref(null)
+function doSomething(){
+  myAction.value = "this works!"
+}
+
+</script>
